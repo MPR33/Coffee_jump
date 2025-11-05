49162014b07f23763f22f6d1a813e09a12aabd88
tayoub95360-jpg <t.ayoub95360@gmail.com>
2025-11-05 08:28:14 +0100

    envoei

--- doodle-jump-2025-10-21-19-24-51/scripts/picots.gd
+++ doodle-jump-2025-10-21-19-24-51/scripts/picots.gd
@@ -1,10 +1,17 @@
 extends "res://scripts/platform.gd"
-@onready var anim:=$anim
+@onready var anim: AnimatedSprite2D = $anim
+@onready var shape: CollisionShape2D = $hitbox/collision
 
-func response():
-	emit_signal("delete_object",self)
-func _ready():
-	anim.play("default")
+func _ready() -> void:           # <- important si le parent fait des choses en _ready
+	anim.frame_changed.connect(_on_frame_changed)
+	anim.play()                 # ou anim.play("le_nom_de_ton_anim") si plusieurs
+	_on_frame_changed()         # init de la hitbox selon la frame
+
+func _on_frame_changed() -> void:
+	# frame 0 = safe → on désactive la hitbox
+	shape.disabled = (anim.frame == 0)
+	# Alternative : $hitbox.monitoring = (anim.frame != 0)
+
 func _on_hitbox_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player") and anim.frame == 2:
+	if body.is_in_group("player"):
 		GameManager._die("Aïe les picots !")
